0805c6edfa6164a21938c6242ed57527c188551a
unknown <C:\Users\Dmitriy\AppData\Roaming\The Bat!>
2021-08-24 17:25:11 +0300

    create card component

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react'
+import './Card.css'
+import star from 'img/star.svg'
+const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+const Card: React.FC = (props: any) => {
+  return <div className="card">
+  <div className="avatar"><img src={props.owner.avatar_url} alt="avatar" /></div>
+    <div className="description">
+      <div className="repoName"><a href={props.html_url}>{props.name}</a></div>
+      <div className="orgName"><a href={props.owner.html_url}>{props.owner.login}</a></div>
+      <div><img src={star} alt="star" />
+        <span>{props.stargazers_count}</span>
+        <span>Updated {new Date(props.updated_at).getDay()} {monthNames[new Date(props.updated_at).getMonth()]}</span>
+      </div>
+    </div>
+  </div>
+}
+
+export default Card
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,9 +1,7 @@
+import Card from 'components/Card/Card'
 import SearchBox from 'components/SearchBox/SearchBox'
-import star from 'img/star.svg'
 import * as React from 'react'
 import './Main.css'
-
-const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
 let start: any[] = []
 
@@ -22,17 +20,7 @@ const Main: React.FC = () => {
   return <div className="grid">
     <SearchBox />
     {repos.map((i) => {
-      return <div className="card">
-      <div className="avatar"><img src={i.owner.avatar_url} alt="avatar" /></div>
-        <div className="description">
-          <div className="repoName"><a href={i.html_url}>{i.name}</a></div>
-          <div className="orgName"><a href={i.owner.html_url}>{i.owner.login}</a></div>
-          <div><img src={star} alt="star" />
-            <span>{i.stargazers_count}</span>
-            <span>Updated {new Date(i.updated_at).getDay()} {monthNames[new Date(i.updated_at).getMonth()]}</span>
-          </div>
-        </div>
-      </div>
+      return  <Card {...i} />
     })}
   </div>
 }
