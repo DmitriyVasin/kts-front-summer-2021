8f875ba4df405fb8841b83dae39f79de310c9236
unknown <C:\Users\Dmitriy\AppData\Roaming\The Bat!>
2021-08-25 11:47:11 +0300

    star vertical-align

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -5,7 +5,7 @@ const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep
 
 const Card: React.FC = (props: any) => {
   return <div className="card">
-  <div className="avatar"><img src={props.owner.avatar_url} alt="avatar" /></div>
+    <div className="avatar"><img src={props.owner.avatar_url} alt="avatar" /></div>
     <div className="description">
       <div className="repoName"><a href={props.html_url}>{props.name}</a></div>
       <div className="orgName"><a href={props.owner.html_url}>{props.owner.login}</a></div>
